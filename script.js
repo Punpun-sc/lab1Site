edfeb598e4e095e7c7dfcb72ee1ca786598a11e4
Seppe Conaerts <seppe@Seppes-MacBook-Air.local>
2025-12-08 18:08:44 +0100

    js added for header/nav

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+function openOverlay() {
 
+}
+let nav = document.getElementById("slideoutNav");
+let hamburger = document.getElementById("hamburger");
 
-function openOverlay(){
-    console.log("pressed")
+function closeOverlay() {
+    nav.style.visibility = "hidden";
+    hamburger.checked = false;
+}
+
+function openNav() {
+    if (hamburger.checked) {
+        nav.style.visibility = "visible";
+    } else {
+        nav.style.visibility = "hidden";
+    }
 }
